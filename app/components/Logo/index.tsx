7701d658bf1aaf0b0e0c979a12e13e7deada0b9c
wx-hanxh3 <kanxh@strongdata.com.cn>
2024-09-10 17:51:32 +0800

    update

--- app/components/Logo/index.tsx
+++ app/components/Logo/index.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 const Logo = () => {
   return (
     <Image
-      src="static/images/home/logo.png"
+      src="static/images/home/logo.jpg"
       alt="logo"
       width={180}
       height={48}
