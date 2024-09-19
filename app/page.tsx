c5240b09e56f902e977dbc55e55b9b06574f7587
wx-hanxh3 <kanxh@strongdata.com.cn>
2024-09-19 18:49:00 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 import OpenAppBtn from '@/components/OpenAppBtn';
+import Address from '@/components/Address';
 export default function Home() {
   return (
     <div className="">
       <OpenAppBtn />
+      <Address color="white" />
     </div>
   );
 }
