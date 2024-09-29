24e615bb0d9226a801c88c6de39a9e02009751d1
wx-hanxh3 <kanxh@strongdata.com.cn>
2024-09-29 10:36:25 +0800

    update

--- next.config.js
+++ next.config.js
@@ -9,8 +9,11 @@ const nextConfig = {
     dirs: ['app'],
   },
   images: {
-    unoptimized: true,
-    // domains: ['static.wixstatic.com', 'assets.zyrosite.com'],
+    // unoptimized: true, // 当为true时，源图像将按原样提供，而不会更改质量、大小或格式
+    domains: [
+      'tuantuan-shop.s3.ap-northeast-1.amazonaws.com',
+      'tuantuan-station.s3.ap-northeast-1.amazonaws.com',
+    ],
     formats: ['image/webp'],
   },
 };
